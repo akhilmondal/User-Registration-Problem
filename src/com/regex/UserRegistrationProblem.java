4d0bbfa1654fb33c8783b503a6c3886fff3977bb
akhilmondal <akhilmandalfamily@gmail.com>
2023-03-21 19:28:13 +0530

    Added: welcome message for the user registration problem.

---
+++ src/com/regex/UserRegistrationProblem.java
@@ -0,0 +1,9 @@
+package com.regex;
+
+public class UserRegistrationProblem {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the User Registration Problem");
+    }
+}
