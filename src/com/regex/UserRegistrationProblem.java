42f6b62ad5c749f2b864b8426487ac7dbcacd794
akhilmondal <akhilmandalfamily@gmail.com>
2023-03-22 19:12:14 +0530

    Added: program to check valid first name.

--- src/com/regex/UserRegistrationProblem.java
+++ src/com/regex/UserRegistrationProblem.java
@@ -1,9 +1,24 @@
 package com.regex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistrationProblem {
+
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to the User Registration Problem");
+        System.out.println("Enter your first name. HINT:-'First name starts with Cap and has minimum 3 characters': ");
+        String firstName = sc.nextLine();
+        /*First name starts with Cap and has minimum 3 characters
+         */
+        boolean result = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", firstName);
+        if (result) {
+            System.out.println("Valid");
+        }else {
+            System.out.println("Invalid");
+        }
     }
 }
